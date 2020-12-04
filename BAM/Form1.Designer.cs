4fec163ca4e50c459911ae88bfc3bc94aa81f23d
jukyung-j <wjdwnrud123@naver.com>
2020-12-04 22:58:01 +0900

    iu, rose

---
+++ BAM/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BAM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bAMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.bAMToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(958, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // bAMToolStripMenuItem
+            // 
+            this.bAMToolStripMenuItem.Name = "bAMToolStripMenuItem";
+            this.bAMToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.bAMToolStripMenuItem.Text = "BAM";
+            this.bAMToolStripMenuItem.Click += new System.EventHandler(this.bAMToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(68, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 230);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(647, 63);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(230, 230);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(359, 192);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(230, 230);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bAMToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ BAM/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(1000, 500);
+            // PictureBox1 속성
+            pictureBox1.Location = new Point(68, 63);
+            pictureBox1.Size = new Size(256, 256);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            // PictureBox2 속성
+            pictureBox2.Location = new Point(647, 63);
+            pictureBox2.Size = new Size(256, 256);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+            // PictureBox3 속성
+            pictureBox3.Location = new Point(359,192);
+            pictureBox3.Size = new Size(256, 256);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.BorderStyle = BorderStyle.FixedSingle;
+        }
+        public byte[,] BitmapToByteArray2D(Bitmap bmp)
+        {
+            byte[,] bmpArray = new byte[bmp.Height * bmp.Width, 3];
+            for (int x = 0; x < bmp.Width; x++)
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color pixelColor = bmp.GetPixel(x, y);
+                    bmpArray[y * bmp.Width + x, 0] = pixelColor.R;
+                    bmpArray[y * bmp.Width + x, 1] = pixelColor.G;
+                    bmpArray[y * bmp.Width + x, 2] = pixelColor.B;
+
+                }
+            return bmpArray;
+        }
+        private int[] hopfieldSetWeight(int[,] image,int[,] image2, int height, int width)
+        {
+            int[,] S = new int[height * width,2];
+            int[] W = new int[height * width];
+            int[] result = { 1, -1 };    // 아이유 1, 로제 -1 결과값
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    S[i * width + j,0] = image[i, j];
+                    S[i * width + j,1] = image2[i, j];
+                }
+
+            }
+            for (int n = 0; n < 2; n++) {
+                for (int i = 0; i < height * width; i++)
+                {
+                    W[i] += S[i,n] * result[n];
+                }
+            }
+            return W;
+        }
+        public int hopfieldPredict(int[,] image, int[,]t_img1,int[,] t_img2,int[] W, int height, int width)
+        {
+            int[] Y = new int[width * height];
+            int[,] S = new int[width * height, 2];
+            int result=0;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Y[i * width + j] = image[i, j];
+                    S[i * width + j, 0] = t_img1[i,j];
+                    S[i * width + j, 1] = t_img2[i,j];
+                }
+            }
+            int[] new_Y = new int[height * width];
+            int u=0, r = 0;
+            bool update = true;
+           
+                for (int i = 0; i < height * width; i++)
+                {
+                    int net = 0;
+                    int[] X = new int[width*height];
+                    for (int j = 0; j < height * width; j++)
+                    {
+                        net += W[j] * Y[j];
+                    }
+                    int new_y = 0;
+                    if (net < 0)
+                    {
+                        new_y = -1;
+                    }
+                    else if (net > 0)
+                    {
+                        new_y = 1;
+                    }
+                    else
+                    {
+                        new_y = Y[i];
+                    }
+                    result = new_y;
+                }
+            
+            
+            return result;
+    }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();     // 클래스 객체 생성 직접 코딩, 속도 메모리공간 효율
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
+            open.Title = "학습할 이미지 선택";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                // 화면에 보이기
+                pictureBox1.Image = new Bitmap(open.FileName);  // Bitmap 객체 생성 imagebox에 집어넣기
+            }
+            open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
+            open.Title = "학습할 이미지 선택";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                // 화면에 보이기
+                pictureBox2.Image = new Bitmap(open.FileName);  // Bitmap 객체 생성 imagebox에 집어넣기
+            }
+
+        }
+
+        private void bAMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "예측할 이미지 선택";
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox3.Image = new Bitmap(open.FileName);
+            }
+
+            Bitmap img = (Bitmap)pictureBox1.Image;
+            Bitmap img2 = (Bitmap)pictureBox2.Image;
+            Bitmap test_img3 = (Bitmap)pictureBox3.Image;
+            int height = img.Height;
+            int width = img.Width;
+
+            byte[,] imgArray = BitmapToByteArray2D(img);    // r,g,b 1Byte씩이라서 메모리공간을 효율적으로 하기 위해
+            byte[,] imgArray2 = BitmapToByteArray2D(img2);
+            byte[,] imgArray3 = BitmapToByteArray2D(test_img3);
+
+            // 1. 학습 이미지 초기화(이진화 포함)
+            int[,] learn_img = new int[height, width];
+            int[,] learn_img2 = new int[height, width];
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    learn_img[i, j] = imgArray[i * width + j, 0] > 128 ? 1 : -1;
+                    learn_img2[i, j] = imgArray2[i * width + j, 0] > 128 ? 1 : -1;
+                }
+
+            int[] W = hopfieldSetWeight(learn_img, learn_img2, height, width);
+
+            int[,] target_img = new int[height, width];
+
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    target_img[i, j] = imgArray3[i * width + j, 0] > 128 ? 1 : -1;
+
+            // 4. 학습된 결과 이미지 예측
+            int result;
+            result = hopfieldPredict(target_img,learn_img,learn_img2, W, height, width);
+            if (result == 1)
+                MessageBox.Show("아이유 입니다");
+            else if (result == -1)
+                MessageBox.Show("로제입니다");
+            else
+                MessageBox.Show("error");
+
+        }
+    }
+}
