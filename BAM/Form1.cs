45e09013d7683a69afcb421f5cc7bb24ef3ffc63
jukyung-j <wjdwnrud123@naver.com>
2020-12-07 01:24:55 +0900

    complete

--- BAM/Form1.cs
+++ BAM/Form1.cs
@@ -32,33 +32,33 @@ namespace BAM
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox3.BorderStyle = BorderStyle.FixedSingle;
         }
-        public byte[,] BitmapToByteArray2D(Bitmap bmp)
+        public byte[] BitmapToByteArray2D(Bitmap bmp)
         {
-            byte[,] bmpArray = new byte[bmp.Height * bmp.Width, 3];
+            byte[] bmpArray = new byte[bmp.Height * bmp.Width];
+            int brightness;
+            Color gray;
             for (int x = 0; x < bmp.Width; x++)
                 for (int y = 0; y < bmp.Height; y++)
                 {
                     Color pixelColor = bmp.GetPixel(x, y);
-                    bmpArray[y * bmp.Width + x, 0] = pixelColor.R;
-                    bmpArray[y * bmp.Width + x, 1] = pixelColor.G;
-                    bmpArray[y * bmp.Width + x, 2] = pixelColor.B;
-
+                    brightness = (int)(0.299 * pixelColor.R + 0.587 * pixelColor.G + 0.114 * pixelColor.B); // gray 변환
+                    gray = Color.FromArgb(brightness, brightness, brightness);
+                    bmp.SetPixel(x, y, gray);
+                    pixelColor = bmp.GetPixel(x, y);
+                    bmpArray[y * bmp.Width + x] = pixelColor.R;
                 }
             return bmpArray;
         }
-        private int[] hopfieldSetWeight(int[,] image,int[,] image2, int height, int width)
+        private int[] hopfieldSetWeight(int[] image,int[] image2, int height, int width)
         {
             int[,] S = new int[height * width,2];
             int[] W = new int[height * width];
             int[] result = { 1, -1 };    // 아이유 1, 로제 -1 결과값
 
-            for (int i = 0; i < height; i++)
+            for (int i = 0; i < height*width; i++)
             {
-                for (int j = 0; j < width; j++)
-                {
-                    S[i * width + j,0] = image[i, j];
-                    S[i * width + j,1] = image2[i, j];
-                }
+                S[i,0] = image[i];
+                S[i,1] = image2[i];
 
             }
             for (int n = 0; n < 2; n++) {
@@ -69,45 +69,57 @@ namespace BAM
             }
             return W;
         }
-        public int hopfieldPredict(int[,] image, int[,]t_img1,int[,] t_img2,int[] W, int height, int width)
+        public int hopfieldPredict(int[] image,int[] W, int height, int width)
         {
             int[] Y = new int[width * height];
+            int[] X = new int[width * height];
             int[,] S = new int[width * height, 2];
-            int result=0;
+            int result = 0 ;
 
-            for (int i = 0; i < height; i++)
+            for (int i = 0; i < height*width; i++)
             {
-                for (int j = 0; j < width; j++)
-                {
-                    Y[i * width + j] = image[i, j];
-                    S[i * width + j, 0] = t_img1[i,j];
-                    S[i * width + j, 1] = t_img2[i,j];
-                }
+                 Y[i] = image[i];
             }
             int[] new_Y = new int[height * width];
-            
-            int net = 0;
-            int[] X = new int[width * height];
-            for (int j = 0; j < height * width; j++)
-            {
-                net += W[j] * Y[j];
+
+            for(int z = 0; z < 2; z++) {
+                int net = 0;
+                for (int j = 0; j < height * width; j++)
+                {
+                    net += W[j] * Y[j];
+                }
+                int new_y = 0;
+                if (net < 0)
+                {
+                    new_y = -1;
+                }
+                else if (net > 0)
+                {
+                    new_y = 1;
+                }
+                else
+                {
+                    new_y = 0;      // 다른 사람
+                    result = new_y;
+                    break;
+                }
+                for (int j = 0; j < height * width; j++)
+                {
+                    X[j] = W[j] * new_y;
+                    if (X[j] > 0)
+                        X[j] = 1;
+                    else if (X[j] < 0)
+                        X[j] = -1;
+                    else
+                        X[j] = Y[j];
+                    Y[j] = X[j];
+                }
+                result = new_y;
             }
-            int new_y = 0;
-            if (net < 0)
-            {
-                new_y = -1;
-            }
-            else if (net > 0)
-            {
-                new_y = 1;
-            }
-            else
-            {
-                new_y = 0;      // 다른 사람
-            }
-            result = new_y;
+           
             return result;
     }
+       
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();     // 클래스 객체 생성 직접 코딩, 속도 메모리공간 효율
@@ -145,31 +157,32 @@ namespace BAM
             int height = img.Height;
             int width = img.Width;
 
-            byte[,] imgArray = BitmapToByteArray2D(img);    // r,g,b 1Byte씩이라서 메모리공간을 효율적으로 하기 위해
-            byte[,] imgArray2 = BitmapToByteArray2D(img2);
-            byte[,] imgArray3 = BitmapToByteArray2D(test_img3);
+            byte[] imgArray = BitmapToByteArray2D(img);    // r,g,b 1Byte씩이라서 메모리공간을 효율적으로 하기 위해
+            byte[] imgArray2 = BitmapToByteArray2D(img2);
+            byte[] imgArray3 = BitmapToByteArray2D(test_img3);
 
             // 1. 학습 이미지 초기화(이진화 포함)
-            int[,] learn_img = new int[height, width];
-            int[,] learn_img2 = new int[height, width];
-            for (int i = 0; i < height; i++)
-                for (int j = 0; j < width; j++)
-                {
-                    learn_img[i, j] = imgArray[i * width + j, 0] > 128 ? 1 : -1;
-                    learn_img2[i, j] = imgArray2[i * width + j, 0] > 128 ? 1 : -1;
-                }
+            int[] learn_img = new int[height*width];
+            int[] learn_img2 = new int[height*width];
+            for (int i = 0; i < height*width; i++)
+            { 
+                learn_img[i] = imgArray[i] > 128 ? 1 : -1;
+                learn_img2[i] = imgArray2[i] > 128 ? 1 : -1;
+             }
 
             int[] W = hopfieldSetWeight(learn_img, learn_img2, height, width);
 
-            int[,] target_img = new int[height, width];
+            int[] target_img = new int[height*width];
 
-            for (int i = 0; i < height; i++)
-                for (int j = 0; j < width; j++)
-                    target_img[i, j] = imgArray3[i * width + j, 0] > 128 ? 1 : -1;
+            for (int i = 0; i < height*width; i++) 
+                 target_img[i] = imgArray3[i] > 128 ? 1 : -1;
 
+           
             // 4. 학습된 결과 이미지 예측
             int result;
-            result = hopfieldPredict(target_img,learn_img,learn_img2, W, height, width);
+
+            result = hopfieldPredict(target_img, W, height, width);
+
             if (result == 1)
                 MessageBox.Show("아이유 입니다");
             else if (result == -1)
